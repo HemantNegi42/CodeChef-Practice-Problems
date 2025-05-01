7f61531a5ba95c221070f2bd73c46e387d94e4e6
Hemant Negi <140361940+HemantNegi42@users.noreply.github.com>
2025-05-01 21:50:05 +0530

    Create cricket-world-cup-qualifier.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/cricket-world-cup-qualifier.cpp
@@ -0,0 +1,16 @@
+/* The cricket World Cup has started in Chefland. There are many teams participating in the group stage matches.
+Any team that scores 12 or more points in the group stage matches qualifies for the next stage.
+
+You know the score that a particular team has scored in the group stage matches. 
+Determine if the team has qualified for the next stage or not. */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    if (x >= 12) cout << "Yes";
+    else cout << "No";
+
+}
