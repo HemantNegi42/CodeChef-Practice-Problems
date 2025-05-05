9ca2de7c9aac98ac380277df6ac58403eec89fc5
Hemant Negi <140361940+HemantNegi42@users.noreply.github.com>
2025-05-05 20:17:11 +0530

    Create lucky-seven.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/lucky-seven.cpp
@@ -0,0 +1,14 @@
+/* Chef considers the number 7 lucky. As a result, he believes that the 
+7-th letter he sees on a day is his lucky letter of the day.
+You are given a string S of length 10, denoting the first 10 letters Chef saw today.
+What is Chef's lucky letter? */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << s[6];
+    return 0;
+}
