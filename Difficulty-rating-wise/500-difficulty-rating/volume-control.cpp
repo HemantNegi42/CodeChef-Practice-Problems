719c9a4f45438f39ba558ec232d69a37e779cac0
Hemant Negi <140361940+HemantNegi42@users.noreply.github.com>
2025-06-20 21:17:15 +0530

    Create volume-control.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/volume-control.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int x, y;
+        cin >> x >> y;
+        cout << abs(x - y) << endl;
+    }
+    return 0;
+}
